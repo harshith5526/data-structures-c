de5d860b10cc969f5228b356915835b26db86847
Harshith A K <harshithak5520@gmail.com>
2026-01-24 20:29:44 +0530

    Add postfix expression evaluation in Suffix-operand.c
    Implement postfix expression evaluation using a stack.

---
+++ expression-operation/Suffix-operand.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ctype.h>
+int i, top = -1;
+int op1, op2, res, s[20];
+char postfix[90], symb;
+void push(int item)
+{
+ top = top+1;
+ s[top] = item;
+}
+int pop()
+{
+ int item;
+ item = s[top];
+ top = top-1;
+ return item;
+}
+void main()
+{
+ printf("\n Enter a valid postfix expression: \n"); else
+ {
+ op2 = pop();
+ op1 = pop();
+ switch(symb)
+ {
+ case '+': push(op1+op2);
+ break;
+ case '-': push(op1-op2);
+ break;
+ case '*': push(op1*op2);
+ break;
+ case '/': push(op1/op2);
+ break;
+ case '%': push(op1%op2);
+ break;
+ case '^': push(pow(op1, op2));
+ break;
+ default : push(0);
+ }
+ }
+ }
+ res = pop();
+ printf("\n Result = %d", res);
+}
